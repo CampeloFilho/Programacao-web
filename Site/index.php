ea16dc5a1c841b11f6b714a2b027a49a82c553c5
CampeloFilho <90098981+CampeloFilho@users.noreply.github.com>
2022-03-10 11:57:55 -0300

    bootsrtrap

--- Site/index.php
+++ Site/index.php
@@ -1,28 +1,27 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>HOME - RAIMUNDO NONATO</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="estilo.css"/> 
-    </head>
-    <body>
-        <h1>EEEP JOAQUIM ANTONIO ALBANO</h1>        
-        <div class="formulario">
-            <form action="#" method="post">
-                Nome:<br><input type="name" name="nome"><br>
-                Sexo:<br><input type="radio" name="sexo" value="masc"> Masculino<BR>
-                <input type="radio" name="sexo" value="fem"> Feminino<BR>
-                Idade: <br><input type="number" max="100" min="1" name="idade"><br>
-                Sua cidade:<br><select name="cidade">
-                    <option value="#">selecione...</option>
-                    <option value="Fortaleza">Fortaleza</option>
-                    <option value="Boa viagem">Boa viagem</option>
-                    <option value="Caucaia">Caucaia</option>
-                    <option value="Eusebio">Eusebio</option>
-                </select><br>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                <input type="submit" name="cadastrar"><br>
-            </form>
-        </div>
-    </body>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
 </html>
